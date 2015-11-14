63cb240635cdc443d4916d6d7430f7d03a41aa7d
sameedo <ismaeelsameed@hotmail.com>
2015-11-14 15:48:52 +0200

    My Django Girls app, first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url (r'^blogs', 'main.views.post_list', name='post_list'),
+
+    url (r'^admin/',include(admin.site.urls)),
+]
