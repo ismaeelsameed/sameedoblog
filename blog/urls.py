075d00029ed43bb2f4211d3f5e952d85259b3411
sameedo <ismaeelsameed@hotmail.com>
2015-11-14 17:27:13 +0200

    'asd'

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url (r'^$', 'main.views.post_list', name='post_list'),
+    url(r'^blogs', 'main.views.post_list', name='post_list'),
 
     url (r'^admin/',include(admin.site.urls)),
 ]
